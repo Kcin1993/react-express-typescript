f4d8cccec4ad415eb84013d94b5a9588cbf4320c
kcin <kcin1993@gmail.com>
2018-11-05 09:21:34 +0800

    init

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+import * as path from "path";
+import * as bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+app.listen(9000);
